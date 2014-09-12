f085f8d7f45c03643eb46484b7a9a1334377fcc4
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-12 14:27:02 -0700

    program to interface example

--- ch1.better_design.rb
+++ ch1.better_design.rb
@@ -4,3 +4,18 @@
 # 2. Program to interface, not implementation
 # 3. Prefer composition over inheritance
 # 4. Delegate, delegate, delegate!
+
+# Bad
+my_car = Car.new
+my_car.drive(200)
+if is_car
+  my_car = Car.new
+  my_car.drive(200)
+else
+  my_plane = AirPlane.new
+  my_plane.fly(200)
+end
+
+# Good
+my_vehicle = get_vehicle
+my_vehicle.travel(200)
