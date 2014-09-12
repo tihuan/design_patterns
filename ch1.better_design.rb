9f7a4c397d764f605f5245a0067e20bda8f4d883
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-12 14:22:22 -0700

    add a word

--- ch1.better_design.rb
+++ ch1.better_design.rb
@@ -1,6 +1,6 @@
 # Better Programs with Design Patterns
 # Four Rules:
-# 1. Separate out the things that change from the rest
+# 1. Separate out the things that are likely to change from the rest
 # 2. Program to interface, not implementation
 # 3. Prefer composition over inheritance
 # 4. Delegate, delegate, delegate!
