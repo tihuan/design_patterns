1a9902b033bb50a84d54d9f969771b443483b24c
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-12 15:23:24 -0700

    add example of composition

--- ch1.better_design.rb
+++ ch1.better_design.rb
@@ -19,3 +19,28 @@ end
 # Good
 my_vehicle = get_vehicle
 my_vehicle.travel(200)
+
+# Another example using car
+class Engine
+  # All sorts of engin-related code...
+
+  def start
+    # Start the engine
+  end
+
+  def stop
+    # Stop the engine
+  end
+end
+
+class Car
+  def initialize
+    @engine = Engine.new
+  end
+
+  def sunday_drive
+    @engine.start
+    # Cruise out into the country and return...
+    @engine.stop
+  end
+end
