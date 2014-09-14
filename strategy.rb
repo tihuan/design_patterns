681005b980cbc19c47142887134f74d5fcca1715
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-13 18:20:00 -0700

    add strategy.rb. add class Formatter and subclass HTMLFormatter

---
+++ strategy.rb
@@ -0,0 +1,21 @@
+# Delegate!!!
+class Formatter
+  def output_report(title, text)
+    raise "Abstract method called"
+  end
+end
+
+class HTMLFormatter < Formatter
+  def output_report(title, text)
+    puts '<html>'
+    puts '  <head>'
+    puts "    <title>#{title}</title>"
+    puts '  </head>'
+    puts '  <body>'
+    text.each do |line|
+      puts "    <p>#{line}</p>"
+    end
+    puts ' </body>'
+    puts '</html>'
+  end
+end
