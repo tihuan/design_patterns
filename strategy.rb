236723f64d8f28c287e5d2f83d5b95b036c98433
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-13 19:18:15 -0700

    same stratgy pattern but use Proc/Lambda instead

--- strategy.rb
+++ strategy.rb
@@ -29,6 +29,22 @@ class Report
   attr_reader :title, :text
   attr_accessor :formatter
 
+  def initialize(formatter)
+    @title = 'Monthly Report'
+    @text = ['Things are going', 'really, really well.']
+    @formatter = formatter
+  end
+
+  def output_report
+    @formatter.output_report(self)
+  end
+end
+
+# Same thing using Proc
+class Report
+  attr_reader :title, :text
+  attr_accessor :formatter
+
   def initialize(&formatter)
     @title = 'Monthly Report'
     @text = ['Things are going', 'really, really well.']
@@ -39,3 +55,21 @@ class Report
     @formatter.call(self)
   end
 end
+
+# class HTMLFormatter now becomes a Lambda
+HTML_FORMATTER = lambda do |context|
+  puts '<html>'
+  puts '  <head>'
+  puts "    <title>#{context.title}</title>"
+  puts '  </head>'
+  puts '  <body>'
+  context.text.each do |line|
+    puts "    <p>#{line}</p>"
+  end
+  puts ' </body>'
+  puts '</html>'
+end
+
+# Generate report
+report = Report.new(&HTML_FORMATTER)
+report.output_report
