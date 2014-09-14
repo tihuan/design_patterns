5b5e688f174869daaeb979cd376a479b9df43077
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-13 23:29:19 -0700

    extract observer code into a module

--- observer.rb
+++ observer.rb
@@ -45,3 +45,24 @@ end
 
 tax_man = TaxMan.new
 fred.add_observer(tax_man)
+
+# Extract observer code into a module
+module Subject
+  def initialize
+    @observers = []
+  end
+
+  def add_observer(observer)
+    @observers << observer
+  end
+
+  def delete_observer(observer)
+    @observers.delete(observer)
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+end
