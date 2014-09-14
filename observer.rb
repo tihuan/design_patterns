2f470db696c10a237b9520830786be283d01b06c
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-13 23:16:05 -0700

    create an employee example and add an observer class

--- observer.rb
+++ observer.rb
@@ -30,3 +30,18 @@ class Employee
     @observers.delete(observer)
   end
 end
+
+# Create an employee
+fred = Employee.new('Fred', 'Crane Operator', 30000.0)
+payroll = Payroll.new
+fred.salary = 35000.0
+
+# An observer
+class TaxMan
+  def update(changed_employee)
+    puts "Send #{changed_employee.name} a new tax bill!"
+  end
+end
+
+tax_man = TaxMan.new
+fred.add_observer(tax_man)
