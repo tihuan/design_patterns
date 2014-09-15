94363f4aac31ee6691e8cdb4cb97c8804ed2f854
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-14 18:28:49 -0700

    add first leaf class example: AddDryIngredients

--- Composite.rb
+++ Composite.rb
@@ -10,3 +10,14 @@ class Task
     0.0
   end
 end
+
+# two leaf classes
+class AddDryIngredientsTask < Task
+  def initialize
+    super('Add dry ingredients')
+  end
+
+  def get_time_required
+    1.0 # 1 minute to add flour and sugar
+  end
+end
