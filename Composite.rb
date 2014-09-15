2a1cfb95d4c60ae057d269adb1dc2f693ea9f912
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-15 15:11:12 -0700

    update #add_sub_task and #remove_sub_task

--- Composite.rb
+++ Composite.rb
@@ -43,10 +43,12 @@ class CompositeTask < Task
 
   def add_sub_task(task)
     @sub_tasks << task
+    task.parent = self
   end
 
   def remove_sub_task(task)
     @sub_tasks.delete(task)
+    task.parent = nil
   end
 
   def get_time_required
