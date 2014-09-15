b364a6390b57325880561e1eb718775d8ca07c10
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-15 15:00:36 -0700

    extract composite task and refactor MakeBatterTask

--- Composite.rb
+++ Composite.rb
@@ -33,13 +33,11 @@ class MixTask < Task
 end
 
 # Composite task
-class MakeBatterTask < Task
-  def initialize
-    super('Make batter')
+
+class CompositeTask < Task
+  def initialize(name)
+    super(name)
     @sub_tasks = []
-    add_sub_task(AddDryIngredientsTask.new)
-    add_sub_task(AddLiquidsTask.new)
-    add_sub_task(MixTask.new)
   end
 
   def add_sub_task(task)
@@ -53,6 +51,15 @@ class MakeBatterTask < Task
   def get_time_required
     time = 0.0
     @sub_tasks.each { |task| time += task.get_time_required }
-    time
+  end
+end
+
+# MakeBatter Task < CompositeTask
+class MakeBatterTask < CompositeTask
+  def initialize
+    super('Make batter')
+    add_sub_task(AddDryIngredientsTask.new)
+    add_sub_task(AddLiquidsTask.new)
+    add_sub_task(MixTask.new)
   end
 end
