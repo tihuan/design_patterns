e00f8258293e1e0dcb813a5a8d39f62ee4ffe962
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-15 16:26:50 -0700

    define class Portfolio

--- Iterator.rb
+++ Iterator.rb
@@ -74,3 +74,18 @@ class Account
     blaance <=> other.balance
   end
 end
+
+class Portfolio
+  include Enumerable
+   def initialize
+    @accounts = []
+   end
+
+   def each(&block)
+    @accounts.each(&block)
+   end
+
+   def add_account(account)
+    @accounts << account
+   end
+end
