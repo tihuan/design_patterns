d2ce38e70b051072d95111bdddb95c35c8f88bd2
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-16 09:11:01 -0700

    add EmployeeManager

--- command.rb
+++ command.rb
@@ -139,3 +139,27 @@ class Employee
     "Employee: name: #{name}, num: #{number}, addr: #{address}"
   end
 end
+
+class EmployeeManager
+  def initialize
+    @employees = {}
+  end
+
+  def add_employee(e)
+    @employees[e.number] = e
+  end
+
+  def change_address(number, address)
+    employee = @employee[number]
+    raise "No such employee" if not employee
+    employee.address = address
+  end
+
+  def delete_employee(number)
+    @employees.remove(number)
+  end
+
+  def find_employee(number)
+    @employee[number]
+  end
+end
