e5e2072fc483da0d6cd8210017c2b3658ed64a1d
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-16 09:23:01 -0700

    finish creating command objects

--- command.rb
+++ command.rb
@@ -175,3 +175,36 @@ class AddEmployee
     system.add_employee(@employee)
   end
 end
+
+class DeleteEmployee
+  def initialize(number)
+    @number = number
+  end
+
+  def execute(system)
+    system.delete_employee(@number)
+  end
+end
+
+class ChangeAddress
+  def initialize(number, address)
+    @number = number
+    @address = address
+  end
+
+  def execute(system)
+    system.change_address(@number, @address)
+  end
+end
+
+class FindEmployee
+  def initialize(number)
+    @number = number
+  end
+
+  def execute(system)
+    system.find_employee(@number)
+  end
+end
+
+
