b3d5299833c2aea5e505294a22556a5afda94bc9
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-16 10:01:47 -0700

    add ^ operator explanation

--- adapter.rb
+++ adapter.rb
@@ -8,9 +8,16 @@ class Encrypter
     key_index = 0
     while not reader.eof?
       clear_char = reader.getc
+      # Use ^ bit operator (XOR*) to encrypt each char
       encrypted_char = clear_char ^ @key[key_index]
       writer.putc(encrypted_char)
       key_index = (key_index + 1) % @key.size
     end
   end
 end
+
+# ^ bit operator only copies the bit if it's set in one operand but not both.
+# Example:
+# a = 0011 1100
+# b = 0000 1101
+# a ^ b = 0011 0001
