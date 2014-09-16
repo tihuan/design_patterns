adaaccbf08d421be9d2a3bf8ef5da14280f2fc26
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-16 09:08:26 -0700

    add Command example: Employee

--- command.rb
+++ command.rb
@@ -120,3 +120,22 @@ cmds.add_command(DeleteFile.new('file1.txt'))
 
 cmds.execute
 puts cmds.description
+
+# Command example with Madeleine
+# New Exmployee class
+require 'rubygem'
+require 'madeleine'
+
+class Employee
+  attr_accessor :name, :number, :address
+
+  def initialize(name, number, address)
+    @name = name
+    @number = number
+    @address = address
+  end
+
+  def to_s
+    "Employee: name: #{name}, num: #{number}, addr: #{address}"
+  end
+end
