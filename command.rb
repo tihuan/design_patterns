bccb3cc7744e99763febe7a6a203e6b91384d3b5
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-15 18:31:33 -0700

    add command subclass CreateFile

--- command.rb
+++ command.rb
@@ -29,3 +29,18 @@ class Command
   def execute
   end
 end
+
+# New command subclass
+class CreateFile < Command
+  def initialize(path, contents)
+    super("Create file: #{path}")
+    @path = path
+    @contents = contents
+  end
+
+  def execute
+    f = File.open(@path, "w")
+    f.write(@contents)
+    f.close
+  end
+end
