749aee4538f8b20cc785493bece01ce8edf0fa34
Timmy Huang <timmyhuang0404@gmail.com>
2014-09-15 18:37:45 -0700

    add CompositeCommand

--- command.rb
+++ command.rb
@@ -69,3 +69,24 @@ class CopyClass < Command
     FileUtils.copy(@source, @target)
   end
 end
+
+# CompositeCommand to collect all subcommands
+class CompositeCommand < Command
+  def initialize
+    @commands = []
+  end
+
+  def add_command(cmd)
+    @commands << cmd
+  end
+
+  def execute
+    @commands.each { |cmd| cmd.execute }
+  end
+
+  def description
+    description = ""
+    @commands.each { |cmd| description += cmd.description + "\n"}
+    description
+  end
+end
